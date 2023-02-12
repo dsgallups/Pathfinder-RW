1236f7a62ddbf0e2d03d675849d2a7dd10bad6ad
Daniel Gallups <dgallups@purdue.edu>
2023-02-11 19:06:59 -0500

    added some code from previous population

--- src/handlers/dev.rs
+++ src/handlers/dev.rs
@@ -5,15 +5,25 @@ use actix_web::{
 };
 use serde_json::json;
 
-use crate::models::university::{
-    University,
-    NewUniversity,
-    UniversityList
-};
+use crate::{models::{
+        university::{
+            University,
+            NewUniversity,
+            UniversityList
+        },
+        component::{
+            Component,
+            NewComponent,
+            ComponentList
+        }
+}, db_connection::PgPooledConnection};
 
 use crate::db_connection::{ PgPool };
+use diesel::PgConnection;
 use crate::handlers::pg_pool_handler;
+use std::str;
 
+use std::process::{Command, Output};
 
 pub async fn reset_and_pop_db(_req: HttpRequest, pool: web::Data<PgPool>) -> HttpResponse {
 
@@ -23,6 +33,34 @@ pub async fn reset_and_pop_db(_req: HttpRequest, pool: web::Data<PgPool>) -> Htt
             return e;
         }
     };
-    
-    return HttpResponse::Ok().json(json!("{'name': 'hi'}"));
+
+    let reset_output = reset_all_tables();
+    println!("-------------------------------------\nTables Reset! Output:\n\n{}\n-------------------------------------", str::from_utf8(&reset_output.stdout).unwrap());
+
+
+    let mut classes: Vec<Component> = Vec::new();
+    let mut components: Vec<Component> = Vec::new();
+
+    push_classes(&mut pg_pool, &mut classes);
+    push_components(&mut pg_pool, &mut classes);
+
+
+
+    return HttpResponse::Ok().json(json!({"name": "hi"}));
+}
+
+fn reset_all_tables() -> Output {
+    Command::new("diesel")
+        .arg("database")
+        .arg("reset")
+        .output()
+        .expect("Failed to reset tables. (diesel/src/dev.rs)")
+}
+
+fn push_classes(conn: &mut PgConnection, class_components: &mut Vec<Component>) {
+
+}
+
+fn push_components(conn: &mut PgPooledConnection, components: &mut Vec<Component>) {
+
 }
