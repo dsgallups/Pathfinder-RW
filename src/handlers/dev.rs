144941e51a9cd6b91162c58316ce48ec6075cb0b
Daniel Gallups <dgallups@purdue.edu>
2023-02-11 19:57:39 -0500

    added code from previous write

--- src/handlers/dev.rs
+++ src/handlers/dev.rs
@@ -42,7 +42,7 @@ pub async fn reset_and_pop_db(_req: HttpRequest, pool: web::Data<PgPool>) -> Htt
     let mut components: Vec<Component> = Vec::new();
 
     push_classes(&mut pg_pool, &mut classes);
-    push_components(&mut pg_pool, &mut classes);
+    push_components(&mut pg_pool, &mut components);
 
 
 
@@ -58,6 +58,75 @@ fn reset_all_tables() -> Output {
 }
 
 fn push_classes(conn: &mut PgConnection, class_components: &mut Vec<Component>) {
+
+    let classes = vec![("CNIT 15501", 3),
+        ("CNIT 17600", 3),
+        ("CNIT 18000", 3),
+        ("CNIT 24200", 3),
+        ("CNIT 25501", 3),
+        ("CNIT 27000", 3),
+        ("CNIT 27200", 3),
+        ("CNIT 28000", 3),
+        ("CNIT 32000", 3),
+        ("CNIT 48000", 3),
+        ("CNIT 37200", 3),
+        ("CNIT 39200", 3),
+        ("CNIT 31500", 3),
+        ("CNIT 32500", 3),
+        ("CNIT 38000", 3),
+        ("CGT 25600", 3),
+        ("CNIT 32200", 3),
+        ("CNIT 24000", 3),
+        ("CNIT 34500", 3),
+        ("CNIT 34600", 3),
+        ("CNIT 34400", 3),
+        ("CNIT 34000", 3),
+        ("CNIT 34010", 1),
+        ("CNIT 34210", 1),
+        ("CNIT 34220", 2),
+        ("CNIT 37000", 3),
+        ("CNIT 42000", 3),
+        ("CNIT 42200", 3),
+        ("CNIT 45500", 3),
+        ("CNIT 47000", 3),
+        ("CNIT 47100", 3),
+        ("ITSEL 00000", 3),
+        ("CSECSEL 00000", 3),
+        ("SCLA 10100", 3),
+        ("SCLA 10200", 3),
+        ("TECH 12000", 3),
+        ("MA 16010", 4),
+        ("MA 16020", 4),
+        ("OLS 25200", 3),
+        ("TLI 11200", 3),
+        ("COMSEL 00000", 3),
+        ("ECONSEL 00000", 3),
+        ("SCISEL 00000", 4),
+        ("LABSCISEL 00000", 4),
+        ("ACCSEL 00000", 3),
+        ("STATSEL 00000", 3),
+        ("SPEAKSEL 00000", 3),
+        ("WRITINGSEL 00000", 3),
+        ("INTERDISC 00000", 3),
+        ("IET 10400", 3),
+        ("IT 10400", 3),
+        ("TLI 11100", 3),
+        ("TLI 15200", 3),
+        ("PHIL 15000", 3),
+        ("HUMSEL 00000", 3),
+        ("BEHAVSCISEL 00000", 3),
+        ("FOUNDSEL 00000", 3),
+        ("FREE 00000", 3),
+        ("SAADSEL 00000", 3)
+    ];
+
+    for class in classes {
+        
+        //make the classes in the class table
+        let db_class = create_class(conn, class.0, class.1);
+    }
+
+
 
 }
 
