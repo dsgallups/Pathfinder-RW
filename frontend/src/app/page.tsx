21287c4b383fddef686a4db402664ccf5c2b03ce
dsgallups <dgallups@purdue.edu>
2023-02-25 20:33:33 -0500

    basic mui exists

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,43 @@
+'use client';
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { Typography, Button } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors';
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: red[500],
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+    },
+});
+
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <div className={styles.description}>
+        <ThemeProvider theme={theme}>
+            <div className={styles.App}>
+                <Typography variant="h1">This is my app</Typography>
+                <Typography variant="h4">Sup</Typography>
+                <Button
+                    color="secondary"
+                    variant="contained"
+                >
+                    Hello from MUI v5
+                </Button>
+            </div>
+            {/*<div className={styles.description}>
                 <p>
                     Get started by editing&nbsp;
                     <code className={styles.code}>src/app/page.tsx</code>
@@ -85,7 +115,7 @@ export default function Home() {
                         Instantly deploy your Next.js site to a shareable URL with Vercel.
                     </p>
                 </a>
-            </div>
-        </main>
+    </div>*/}
+        </ThemeProvider>
     )
 }
