df1b8e2a12ac0d2a74ae7b4f6405d362a08a0298
dsgallups <dgallups@purdue.edu>
2023-02-25 20:39:27 -0500

    deleted old frontend code and added better favicons

--- frontend-old/src/config/createEmotionCache.tsx
+++
@@ -1,5 +0,0 @@
-import createCache from '@emotion/cache';
-//https://medium.com/nextjs/how-to-use-material-ui-with-nextjs-and-react-18-6c054ceacf77
-export default function createEmotionCache() {
-    return createCache({ key: 'css', prepend: true });
-}
