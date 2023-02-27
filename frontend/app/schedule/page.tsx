0f1f8847d704e70a0a597763985de03c3baa3f25
Daniel Gallups <dgallups@purdue.edu>
2023-02-26 23:08:31 -0500

    added buttons for student nav

--- frontend/app/schedule/page.tsx
+++ frontend/app/schedule/page.tsx
@@ -12,14 +12,19 @@ import {
     Container,
     Box,
     Menu,
-    MenuItem
+    MenuItem,
+    Avatar,
+    Grid,
+    Fab
 } from '@mui/material';
 
 import { IconPropsColorOverrides } from '@mui/material/Icon';
 import { SvgIconPropsColorOverrides } from '@mui/material';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import { ThemeProvider, createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors';
+import { deepOrange, red } from '@mui/material/colors';
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -95,9 +100,15 @@ declare module '@mui/material/Button' {
         selectedText: true;
         unselectedText: true;
         secondary: true;
+        green: true;
     }
 }
-
+declare module '@mui/material/Fab' {
+    interface FabPropsColorOverrides {
+        green: true;
+        yellow: true;
+    }
+}
 
 const views = ["Plans", "Grades", "Graphs"];
 
@@ -187,9 +198,26 @@ export default function Home() {
                                 onClick={handleClick}
                                 sx={{
                                     textTransform: "none",
+                                    borderRadius: "10px"
+
                                 }}
                             >
-                                Daniel Gallups
+                                <Avatar sx={{
+                                    width: 18,
+                                    height: 18,
+                                    bgcolor: deepOrange[500],
+                                    mr: 1,
+                                    mb: .5
+                                }}>DG</Avatar>
+                                <Typography
+                                    variant="subtitle1"
+                                    sx={{
+                                        fontWeight: 500
+                                    }}
+                                >
+                                    Daniel Gallups
+                                </Typography>
+
                             </Button>
                             <Menu
                                 id="basic-menu"
@@ -207,8 +235,127 @@ export default function Home() {
 
                     </Toolbar>
                 </Container>
+                <Grid2
+                    container
+                    sx={{
+                        '--Grid-borderWidth': '1px',
+                        borderTop: 'var(--Grid-borderWidth) solid',
+                        borderLeft: 'var(--Grid-borderWidth) solid',
+                        borderColor: 'divider',
+                        '& > div': {
+                            borderRight: 'var(--Grid-borderWidth) solid',
+                            borderBottom: 'var(--Grid-borderWidth) solid',
+                            borderColor: 'divider',
+                        },
+                    }}
+                >
+                    <Grid2
 
-                <Typography variant="h1">This is my app</Typography>
+                        xs
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "flex-start",
+                            backgroundColor: "primary.main",
+                            justifyContent: "space-between",
+                            p: 2,
+
+                        }}>
+                        <Grid2 container sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                        }}>
+
+                            <Typography sx={{ fontWeight: "700" }}>Current Plan</Typography>
+                            <Typography>Fall 2020 - Fall 2022</Typography>
+
+
+
+                        </Grid2>
+                        <UnfoldMoreIcon sx={{ alignSelf: "center" }} />
+                    </Grid2>
+
+
+                    <Grid2 xs sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "flex-start",
+                        backgroundColor: "primary.main",
+                        p: 2,
+                    }}>
+                        <Button
+                            variant="contained"
+                            color="green"
+                            disableElevation
+                            sx={{
+                                textTransform: "none",
+                                color: "white",
+                                fontWeight: "700",
+                                fontSize: ".8rem",
+                                borderRadius: "25px",
+                                alignItems: "center",
+                                mb: 0,
+                                pt: .5,
+                                pb: .2,
+                                mr: 2,
+                            }}
+                        >Major</Button>
+                        <Grid2 container sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                        }}>
+                            <Typography sx={{ fontWeight: "700" }}>Cybersecurity</Typography>
+                            <Typography variant="subtitle2" sx={{ color: "unselectedText.main" }}>& Marketing</Typography>
+                        </Grid2>
+                    </Grid2>
+
+                    <Grid2 xs sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "flex-start",
+                        backgroundColor: "primary.main",
+                        p: 2,
+                    }}>
+                        <Button
+                            variant="contained"
+                            color="green"
+                            disableElevation
+                            sx={{
+                                textTransform: "none",
+                                color: "white",
+                                fontWeight: "700",
+                                fontSize: ".8rem",
+                                borderRadius: "25px",
+                                alignItems: "center",
+                                mb: 0,
+                                pt: .5,
+                                pb: .2,
+                                mr: 2,
+                            }}
+                        >Major</Button>
+                        <Grid2 container sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                        }}>
+                            <Typography sx={{ fontWeight: "700" }}>Cybersecurity</Typography>
+                            <Typography variant="subtitle2" sx={{ color: "unselectedText.main" }}>& Marketing</Typography>
+                        </Grid2>
+                    </Grid2>
+
+
+
+                    <Grid2 xs>
+
+
+                    </Grid2>
+                    <Grid2 xs>
+
+                    </Grid2>
+                </Grid2>
+                <Typography variant="h1">This pathfinder</Typography>
                 <Typography variant="h4">Sup</Typography>
                 <Button
                     color="yellow"
@@ -217,6 +364,6 @@ export default function Home() {
                     Hello from MUI v5
                 </Button>
             </div>
-        </ThemeProvider>
+        </ThemeProvider >
     )
 }
