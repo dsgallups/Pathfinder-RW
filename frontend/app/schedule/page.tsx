49fea832f293a2f8aa04aab1168bf16d80e46d76
Daniel Gallups <dgallups@purdue.edu>
2023-02-26 20:25:01 -0500

    added good styling, created schedule page

--- frontend/app/schedule/page.tsx
+++ frontend/app/schedule/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import styles from '../page.module.css'
 import {
     Typography,
     Button,
